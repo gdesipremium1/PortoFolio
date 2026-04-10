e3b96fa2a7f615445be99ed9f3715eb483abe634
desire.concise.digital <gdesi.mada@gmail.com>
2026-04-10 21:06:55 +0200

    flag falback

--- src/app/components/translate/LanguageSwitcher.tsx
+++ src/app/components/translate/LanguageSwitcher.tsx
@@ -1,17 +1,18 @@
 import i18n from "../../../i18n";
+import { ImageWithFallback } from '../figma/ImageWithFallback';
 
 export default function LanguageSwitcher() {
        return (
               <>
                      <button onClick={() => i18n.changeLanguage("fr")}>
-                            <img
+                            <ImageWithFallback
                                    src="images/flags/fr.png"
                                    alt="Français"
                                    className="w-8 h-8 rounded-full cursor-pointer"
                             />
                      </button>
                      <button onClick={() => i18n.changeLanguage("en")}>
-                            <img
+                            <ImageWithFallback
                                    src="images/flags/en.png"
                                    alt="English"
                                    className="w-8 h-8 rounded-full cursor-pointer"
